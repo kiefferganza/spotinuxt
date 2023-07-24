9c6caee530cca554ad938079c6ca515e8e9c5ed4
Kieffer Ganza <kiefferganza23@gmail.com>
2023-07-24 17:20:40 +0800

    removed dark mode

--- components/layouts/sidebar.vue
+++ components/layouts/sidebar.vue
@@ -1,17 +1,17 @@
 <template>
   <aside
     id="logo-sidebar"
-    class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+    class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0"
     aria-label="Sidebar"
   >
-    <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+    <div class="h-full px-3 pb-4 overflow-y-auto bg-white">
       <ul class="space-y-2 font-medium">
         <li v-for="(item, index) in sidebarItems" :key="index">
           <a
             @click="router.push(item.href)"
-            class="cursor-pointer flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+            class="cursor-pointer flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100"
           >
-            <Icon name="bi:reception-4" color="white" />
+            <Icon name="bi:reception-4" color="black" />
             <span class="ml-3">{{ item.title }}</span>
           </a>
         </li>
